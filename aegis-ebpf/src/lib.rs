60a1bb33ab4a0db0a729cef3510da3743ed3550c
azhar457 <azharsss457@gmail.com>
2026-06-27 10:01:30 +0700

    fix: declare no_std in ebpf lib.rs to prevent std crate error

--- aegis-ebpf/src/lib.rs
+++ aegis-ebpf/src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![no_std]
+// Unused lib file, kept with #![no_std] to prevent cargo compiler from looking for std crate on bpfel target.
